6a95e8f4358971395251b4da689fcdcc94e45d17
sasha <sasha.friis@icloud.com>
2025-02-07 11:50:54 +0100

    done and dusted

---
+++ array.js
@@ -0,0 +1,11 @@
+let person = {
+    firstname: "mikkel",
+    lastname: "prut", 
+    age: "69",
+    occupation: "professional flatulator"
+}
+
+console.log(person.firstname)
+console.log(person.lastname)
+console.log(person["age"])
+console.log(person["occupation"])
